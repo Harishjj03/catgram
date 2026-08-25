8e5d7783796ed23e002b4400213dd0964114050f
Harishjj03 <harishbalajj@gmail.com>
2026-08-26 02:32:35 +0530

    Fix demo login

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     setMessage("");
 
     try {
-      const response = await fetch("https://catgram-1goz.onrender.com/api/login", {
+      const response = await fetch("http://localhost:5000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
